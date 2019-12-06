f8f46d81b185de9b6fe43721fc4c03b6d366a747
Stefan Glase <stefan.glase@googlemail.com>
2019-12-06 11:00:29 +0100

    feat: create subtasks for every line of the provided string

--- src/main/java/de/codescape/jira/plugins/multiplesubtasks/action/MultipleSubTasksDialogAction.java
+++ src/main/java/de/codescape/jira/plugins/multiplesubtasks/action/MultipleSubTasksDialogAction.java
@@ -1,10 +1,28 @@
 package de.codescape.jira.plugins.multiplesubtasks.action;
 
+import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
+import de.codescape.jira.plugins.multiplesubtasks.service.SubTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+/**
+ * TODO add documentation
+ */
 public class MultipleSubTasksDialogAction extends JiraWebActionSupport {
 
     private static final long serialVersionUID = 1L;
+
+    private String issueKey;
+    private List<Issue> createdSubTasks;
+
+    private final SubTaskService subTaskService;
+
+    @Autowired
+    public MultipleSubTasksDialogAction(SubTaskService subTaskService) {
+        this.subTaskService = subTaskService;
+    }
 
     /**
      * Names of all parameters used on the global configuration page.
@@ -25,13 +43,28 @@ public class MultipleSubTasksDialogAction extends JiraWebActionSupport {
         return getHttpRequest().getParameter(parameterName);
     }
 
+    public String getIssueKey() {
+        return issueKey;
+    }
+
+    public List<Issue> getCreatedSubTasks() {
+        return createdSubTasks;
+    }
+
     @Override
     protected String doExecute() {
+        System.out.println("doExecute in " + getClass().getSimpleName() + " called:");
+
+        issueKey = getParameter("issueKey");
+        System.out.println(" ~ issueKey: " + issueKey);
+
         String action = getParameter(Parameters.ACTION);
+        System.out.println(" ~ action: " + action);
         if (action != null && action.equals("create")) {
-            System.out.println("############ CREATE pressed!");
-            System.out.println("issueKey: " + getParameter("issueKey"));
-            System.out.println("tasks: " + getParameter("tasks"));
+            String tasks = getParameter("tasks");
+            System.out.println(" ~ tasks: " + tasks);
+            createdSubTasks = subTaskService.subTasksFromString(issueKey, tasks);
+            createdSubTasks.forEach(System.out::println);
         }
         return SUCCESS;
     }
---
+++ src/main/java/de/codescape/jira/plugins/multiplesubtasks/service/SubTaskService.java
@@ -0,0 +1,85 @@
+package de.codescape.jira.plugins.multiplesubtasks.service;
+
+import com.atlassian.jira.bc.issue.IssueService;
+import com.atlassian.jira.config.SubTaskManager;
+import com.atlassian.jira.exception.CreateException;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.IssueFactory;
+import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.issuetype.IssueType;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+// TODO extract interface
+@Component
+public class SubTaskService {
+
+    private IssueService issueService;
+
+    private IssueFactory issueFactory;
+
+    private IssueManager issueManager;
+
+    private SubTaskManager subTaskManager;
+
+    private JiraAuthenticationContext jiraAuthenticationContext;
+
+    @Autowired
+    public SubTaskService(@ComponentImport IssueService issueService,
+                          @ComponentImport IssueFactory issueFactory,
+                          @ComponentImport IssueManager issueManager,
+                          @ComponentImport SubTaskManager subTaskManager,
+                          @ComponentImport JiraAuthenticationContext jiraAuthenticationContext) {
+        this.issueService = issueService;
+        this.issueFactory = issueFactory;
+        this.issueManager = issueManager;
+        this.subTaskManager = subTaskManager;
+        this.jiraAuthenticationContext = jiraAuthenticationContext;
+    }
+
+    // docs https://community.atlassian.com/t5/Answers-Developer-Questions/Auto-create-subtask-and-assign-to-users/qaq-p/530837
+    public List<Issue> subTasksFromString(String issueKey, String input) {
+        ArrayList<Issue> subTasks = new ArrayList<>();
+
+        IssueService.IssueResult issueResult = issueService.getIssue(jiraAuthenticationContext.getLoggedInUser(), issueKey);
+        MutableIssue parent = issueResult.getIssue();
+        if (parent == null) {
+            throw new RuntimeException("Parent issue not found.");
+        }
+
+        Collection<IssueType> issueTypes = parent.getProjectObject().getIssueTypes();
+        IssueType subTaskType = issueTypes.stream().filter(issueType -> issueType.isSubTask()).findFirst().orElse(null);
+        if (subTaskType == null) {
+            throw new RuntimeException("No sub task types found.");
+        }
+
+        new BufferedReader(new StringReader(input)).lines().forEach(taskSummary -> {
+            MutableIssue newSubTask = issueFactory.getIssue();
+            newSubTask.setSummary(taskSummary);
+            newSubTask.setParentObject(parent);
+            newSubTask.setProjectObject(parent.getProjectObject());
+            newSubTask.setIssueType(subTaskType);
+
+            try {
+                issueManager.createIssueObject(jiraAuthenticationContext.getLoggedInUser(), newSubTask);
+                // TODO do we need to do that?
+                subTaskManager.createSubTaskIssueLink(parent, newSubTask, jiraAuthenticationContext.getLoggedInUser());
+            } catch (CreateException e) {
+                throw new RuntimeException(e);
+            }
+
+            subTasks.add(newSubTask);
+        });
+        return subTasks;
+    }
+
+}
