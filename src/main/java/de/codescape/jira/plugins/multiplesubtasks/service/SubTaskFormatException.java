9899e508464b95516c6d71738d533aacb5475ceb
Stefan Glase <stefan.glase@googlemail.com>
2020-01-22 20:41:19 +0100

    docs: add documentation to format exception

--- src/main/java/de/codescape/jira/plugins/multiplesubtasks/service/SubTaskFormatException.java
+++ src/main/java/de/codescape/jira/plugins/multiplesubtasks/service/SubTaskFormatException.java
@@ -1,12 +1,26 @@
 package de.codescape.jira.plugins.multiplesubtasks.service;
 
-// TODO document class
+/**
+ * Exception class to be used for errors while parsing an input string and trying to extract a single or multiple
+ * requests for new subtasks from it.
+ */
 public class SubTaskFormatException extends RuntimeException {
 
+    /**
+     * Create a new {@link SubTaskFormatException} with just an error message.
+     *
+     * @param message error message
+     */
     public SubTaskFormatException(String message) {
         super(message);
     }
 
+    /**
+     * Create a new {@link SubTaskFormatException} with an error message and a root cause.
+     *
+     * @param message   error message
+     * @param throwable root cause
+     */
     public SubTaskFormatException(String message, Throwable throwable) {
         super(message, throwable);
     }
