8494b5178ffa23f3576aab195d696c5466c19027
Stefan Glase <stefan.glase@googlemail.com>
2023-02-24 11:49:46 +0100

    docs: add Java documentation to model classes

--- docs/changelog.md
+++ docs/changelog.md
@@ -12,7 +12,7 @@ Click on the version in the following list to see all changes since the previous
 
 ### [Unreleased]
 
-* ...
+* docs: add Java documentation to model classes
 
 ### [23.02.1] - 2023-02-19
 
--- src/main/java/de/codescape/jira/plugins/multiplesubtasks/model/CreatedSubtask.java
+++ src/main/java/de/codescape/jira/plugins/multiplesubtasks/model/CreatedSubtask.java
@@ -5,24 +5,43 @@ import com.atlassian.jira.issue.Issue;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The {@link CreatedSubtask} represents a subtask that was created. This object can potentially contain warnings that
+ * occurred during the creation of the subtask in Jira.
+ */
 public class CreatedSubtask {
 
     private final Issue issue;
     private final List<String> warnings;
 
+    /**
+     * Initialize the created subtask.
+     *
+     * @param issue    issue that was created
+     * @param warnings warnings that occurred during creation
+     */
     public CreatedSubtask(Issue issue, List<String> warnings) {
         this.issue = issue;
         this.warnings = warnings;
     }
 
+    /**
+     * Return the issue that was created.
+     */
     public Issue getIssue() {
         return issue;
     }
 
+    /**
+     * Return all warnings that occurred during creation.
+     */
     public List<String> getWarnings() {
         return warnings;
     }
 
+    /**
+     * Return all warnings that occurred during creation in HTML format.
+     */
     public String getWarningsMessage() {
         return warnings.isEmpty() ? null : warnings.stream()
             .collect(Collectors.joining("</li><li>", "<ul><li>", "</li></ul>"));
